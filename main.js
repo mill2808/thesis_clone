1b62ef953446078eb398492d7ac55b82ca999765
mill2808 <105799175+mill2808@users.noreply.github.com>
2022-06-29 20:17:50 -0400

    resource page

--- main.js
+++ main.js
@@ -19,3 +19,22 @@ for (i = 0; i < faq.length; i++) {
     });
 }
 
+/* ********************** Resource dropdown answers ******************************** */
+var resource = document.getElementsByClassName("resource-page");
+var i;
+for (i = 0; i < resource.length; i++) {
+    resource[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+
